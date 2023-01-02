82749e8e6abd13bbc0a0bc403237aa29868c9a70
Maciej Fec <maciejfec1996@gmail.com>
2023-01-02 17:56:21 +0000

    import and use passwords route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express")
+const passwordsRoute = require("./routes/passwords")
 
 if(process.env.NODE_ENV !== "production") require("dotenv").config({ path: "../.env" })
 
 const app = express()
 const PORT = process.env.PORT || process.env.EXPRESS_PORT
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use(express.json())
+app.use("/passwords", passwordsRoute)
 
 app.listen(PORT, () => {
     console.log(`Currently listening on port ${PORT}`)
