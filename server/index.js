42e94934b07759a027e47de0725782cb7985ab10
Maciej Fec <maciejfec1996@gmail.com>
2023-01-03 12:42:35 +0000

    add users route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const passwordsRoute = require("./routes/passwords")
+const usersRoute = require("./routes/users")
 
 if(process.env.NODE_ENV !== "production") require("dotenv").config({ path: "../.env" })
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || process.env.EXPRESS_PORT
 
 app.use(express.json())
 app.use("/passwords", passwordsRoute)
+app.use("/users", usersRoute)
 
 app.listen(PORT, () => {
     console.log(`Currently listening on port ${PORT}`)
