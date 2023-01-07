e7008306f9ffca0c3a697409de4872235ce547a1
Maciej Fec <maciejfec1996@gmail.com>
2023-01-07 21:13:24 +0000

    add db connection

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,12 +5,21 @@ const responses = require("../responses.json")
 const bcrypt = require("bcrypt")
 const getDate = require("../utilities/date")
 const crypto = require("crypto")
+const User = require("../models/User")
+const db = require("../db/DynamoDbDatabase")
 
 const users = []
 
 route.get("/", (req, res) => {
     res.status(200)
-    res.json(users)
+
+    listOfUsers = []
+
+    users.forEach(user => {
+        listOfUsers.push(user.getUser())
+    })
+
+    res.json(listOfUsers)
 })
 
 route.post("/register", dataSantiser.sanitise(), async (req, res) => {
@@ -18,25 +27,34 @@ route.post("/register", dataSantiser.sanitise(), async (req, res) => {
     const id = crypto.randomUUID()
     const email = req.body.email 
     const password = req.body.password
+    const repeatPassword = req.body.repeatPass
     const dateJoined = getDate()
     const blocked = false
 
     if(!email) return res.status(responses.statusCodes.badReq).json({message: responses.messages.emailRequired})
-    if(!password) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordRequired})
+    if(!password || !repeatPassword) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordRequired})
 
-    const foundUser = users.find(user => user.email === req.body.email)
-    
+    const foundUser = users.find(user => user.getEmail() === req.body.email)
     if(foundUser) return res.status(responses.statusCodes.conflict).json({message: responses.messages.userExists})
+
+    if(password !== repeatPassword) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordsMustMatch})
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
-        const user = {id, email, dateJoined, blocked}
-        users.push(user)
+        const user = new User(db.getDynamoDocClient())
+        user.setId(id)
+        user.setEmail(email)
+        user.setPassword(hashedPassword)
+        user.setDateJoined(dateJoined)
+        user.setBlocked(blocked)
+
+        const result = user.create()
+        if(result !== 0) return res.status(responses.statusCodes.internalServerError).json({message: result})
 
         res.status(responses.statusCodes.created)
-        res.json({message: responses.messages.userCreated, user: user})
+        res.json({message: responses.messages.userCreated, user: user.getUser()})
     }
-    catch {
+    catch(err) {
         res.status(responses.statusCodes.internalServerError).json({message: responses.messages.internalServerError})
     }
 })
