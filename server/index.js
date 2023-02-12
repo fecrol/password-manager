4301bfd29f68938a399dbf719f8dc740455ab883
Maciej Fec <maciejfec1996@gmail.com>
2023-02-12 20:11:00 +0000

    rename variables

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express")
-const passwordsRoute = require("./routes/passwords")
+const credentialssRoute = require("./routes/credentials")
 const usersRoute = require("./routes/users")
 
 if(process.env.NODE_ENV !== "production") require("dotenv").config({ path: "../.env" })
 
 const app = express()
-const PORT = process.env.PORT || process.env.EXPRESS_PORT
+const PORT = process.env.PORT
 
 app.use(express.json())
-app.use("/passwords", passwordsRoute)
+app.use("/credentials", credentialssRoute)
 app.use("/users", usersRoute)
 
 app.listen(PORT, () => {
