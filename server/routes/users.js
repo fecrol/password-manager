6c6315d88f32c582e0a18425c6076e3d3e7148c7
Maciej Fec <maciejfec1996@gmail.com>
2023-03-13 13:42:44 +0000

    remove login and register requests

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,7 @@ const getDate = require("../utilities/date")
 const crypto = require("crypto")
 const User = require("../models/User")
 const db = require("../db/DynamoDbDatabase")
-const jwt = require("jsonwebtoken")
+const authenticator = require("../middleware/authenticator")
 
 if(process.env.NODE_ENV !== "production") require("dotenv").config({ path: "../../.env" })
 
@@ -31,64 +31,6 @@ route.get("/users/:id", async (req, res) => {
     if(foundUser.password) delete foundUser.password
 
     res.status(responses.statusCodes.ok).json(foundUser)
-})
-
-route.post("/login", dataSantiser.sanitise(), async (req, res) => {
-    const email = req.body.email
-    const password = req.body.password
-
-    if(!email) return res.status(responses.statusCodes.badReq).json({message: responses.messages.emailRequired})
-    if(!password) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordRequired})
-
-    const user = new User({docClient: db.getDynamoDocClient()})
-    const foundUser = await user.readByEmail(email)
-
-    if(!foundUser) return res.status(responses.statusCodes.badReq).json({message: responses.messages.invalidEmail})
-
-    try {
-        const passwordIsCorrect = await bcrypt.compare(password, foundUser.password)
-        if(!passwordIsCorrect) return res.status(responses.statusCodes.badReq).json({message: responses.messages.invalidPassword})
-
-        const accessToken = jwt.sign({id: foundUser.id}, process.env.ACCESS_TOKEN_SECRET)
-        
-        res.status(responses.statusCodes.created).json({accessToken})
-    } 
-    catch {
-        res.status(responses.statusCodes.internalServerError).json({message: responses.messages.internalServerError})
-    }
-})
-
-route.post("/register", dataSantiser.sanitise(), async (req, res) => {
-
-    const id = crypto.randomUUID()
-    const email = req.body.email 
-    const password = req.body.password
-    const repeatPassword = req.body.repeatPass
-    const dateJoined = getDate()
-    const blocked = false
-
-    if(!email) return res.status(responses.statusCodes.badReq).json({message: responses.messages.emailRequired})
-    if(!password || !repeatPassword) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordRequired})
-
-    const user = new User({docClient: db.getDynamoDocClient()})
-
-    const foundUser = await user.readByEmail(email)
-    if(foundUser) return res.status(responses.statusCodes.conflict).json({message: responses.messages.userExists})
-
-    if(password !== repeatPassword) return res.status(responses.statusCodes.badReq).json({message: responses.messages.passwordsMustMatch})
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        user.setUser({id, email, password: hashedPassword, dateJoined, blocked})
-
-        const result = await user.create()
-        if(!result.user) return res.status(responses.statusCodes.internalServerError).json({message: result})
-
-        res.status(responses.statusCodes.created).json({user: result.user})
-    }
-    catch(err) {
-        res.status(responses.statusCodes.internalServerError).json({message: responses.messages.internalServerError})
-    }
 })
 
 module.exports = route
